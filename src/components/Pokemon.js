e35995a253d8c4129818786af640de40c7916ebd
Azari80 <63757784+Azari80@users.noreply.github.com>
2023-07-01 23:27:51 -0400

    commit

---
+++ src/components/Pokemon.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './../App.css';
+import Imagepok from './Imagepok';
+
+function Pokemon() {
+    return (
+        <div>
+          <Imagepok />
+        </div>
+    )
+}
+
+export default Pokemon
